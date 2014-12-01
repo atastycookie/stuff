1db10b2ece867022d4163b8b29d8f8232cde95c7
Roman Ananyev <aTastyCookie@users.noreply.github.com>
2014-12-01 15:57:15 +0300

    Update backup_multiple_db_to_git.sh
    backups are sorted into folders

--- backup_multiple_db_to_git.sh
+++ backup_multiple_db_to_git.sh
@@ -6,11 +6,13 @@ DB="batabase1 batabase2 batabase3"
 
 for DB_NAME in $DB; do
 
- cd $BACKUP_DIR
-
- DB_DUMP="$BACKUP_DIR/$DB_NAME.sql"
  TIME_BACKUP=$(date +"%m-%d-%Y-%T")
- 
+
+ cd $BACKUP_DIR
+ mkdir -p $BACKUP_DIR/$DB_NAME/$TIME_BACKUP
+ cd $BACKUP_DIR/$DB_NAME/$TIME_BACKUP
+
+ DB_DUMP="$BACKUP_DIR/$DB_NAME/$TIME_BACKUP/$DB_NAME.sql"
  mysqldump -u $DB_USER -p$DB_PASS --skip-extended-insert $DB_NAME > $DB_DUMP
 
  cd $BACKUP_DIR
@@ -18,5 +20,6 @@ for DB_NAME in $DB; do
  git add $DB_DUMP
  git commit -m "Update database dump $TIME_BACKUP"
  git push
+
  
 done
